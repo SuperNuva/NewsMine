76f4eb314eca0c0b250fbfa26f6509d1d8a511ac
Tasnuva Noor <noortasnuva@gmail.com>
2018-04-27 15:46:40 -0400

    select form

--- client/components/preferenceForm.js
+++ client/components/preferenceForm.js
@@ -10,7 +10,20 @@ class PreferenceForm extends Component {
     render() {
         <div>
             <form>
-                
+                <label>Select a country
+                    <select>
+                        <option>Australia</option>
+                        <option>Brazil</option>
+                        <option>Canada</option>
+                        <option>China</option>
+                        <option>Germany</option>
+                        <option>India</option>
+                        <option>Korea, Republic of</option>
+                        <option>South Africa</option>
+                        <option>United Kingdom</option>
+                        <option>United States of America</option>
+                    </select>
+                </label>
             </form>
         </div>
     }
